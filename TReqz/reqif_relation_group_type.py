f4b57c236f0620305a9899679b1b7ee818488195
Till Uhlig <till.uhlig@student.uni-halle.de>
2019-01-08 18:32:29 +0100

    - default dict for __init and decode() fixed - incorrect function call fixed

--- TReqz/reqif_relation_group_type.py
+++ TReqz/reqif_relation_group_type.py
@@ -4,12 +4,12 @@ from .. import TReqz
 
 class reqif_relation_group_type(TReqz.reqif_identifiable):
 
-    def __init__(self, content: Element = None, id_dict={}):
+    def __init__(self, content: Element = None, id_dict=None):
         self.spec_attributes: list = list()  # reqif_attribute_definition, optional
         self.name = "RELATION-GROUP-TYPE"
         super(reqif_relation_group_type, self).__init__(content, id_dict)
 
-    def decode(self, content: Element, id_dict: TReqz.reqif_id_dict = {}):
+    def decode(self, content: Element, id_dict: TReqz.reqif_id_dict = None):
         super().decode(content, id_dict)
         namespace = TReqz.xml_utils.get_tag_namespace(content.tag)
 
@@ -20,7 +20,7 @@ class reqif_relation_group_type(TReqz.reqif_identifiable):
                     "ATTRIBUTE-DEFINITION-REAL": "reqif_attribute_definition_real",
                     "ATTRIBUTE-DEFINITION-STRING": "reqif_attribute_definition_string",
                     "ATTRIBUTE-DEFINITION-XHTML": "reqif_attribute_definition_xhtml"}
-        self.spec_attributes = TReqz.xml_utils.generate_object_list_by_element_class(
+        self.spec_attributes = TReqz.reqif_utils.generate_object_list_by_element_class(
             content, id_dict, "./{0}SPEC-ATTRIBUTES".format(namespace), typeList)
 
     def encode(self):
