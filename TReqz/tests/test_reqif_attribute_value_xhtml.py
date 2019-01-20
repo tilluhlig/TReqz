336bb4910b0679d95cf020ff21142234f3236c58
Till Uhlig <till.uhlig@student.uni-halle.de>
2019-01-20 21:40:27 +0100

    mistake fixed

--- TReqz/tests/test_reqif_attribute_value_xhtml.py
+++ TReqz/tests/test_reqif_attribute_value_xhtml.py
@@ -12,13 +12,13 @@ class TestReqifAttributeValueXhtml(unittest.TestCase):
     def test_decode(self):
         TE.utils.testDecodeLocalRefFromElementText(self, self.obj, "<DEFINITION><ATTRIBUTE-DEFINITION-XHTML-REF>1</ATTRIBUTE-DEFINITION-XHTML-REF></DEFINITION>", "definition", "1")
         TE.utils.testDecodeAttribute(self, self.obj, 'the_value', 'THE-VALUE')
-        TE.utils.testDecodeAttribute(self, self.obj, 'the_original_value', 'THE-ORIGINAL-VALUe')
+        TE.utils.testDecodeAttribute(self, self.obj, 'the_original_value', 'THE-ORIGINAL-VALUE')
         TE.utils.testDecodeAttribute(self, self.obj, 'is_simplified', 'IS-SIMPLIFIED')
 
     def test_encode(self):
         TE.utils.testEncodeLocalRefFromElementText(self, self.obj, "<DEFINITION><ATTRIBUTE-DEFINITION-XHTML-REF>1</ATTRIBUTE-DEFINITION-XHTML-REF></DEFINITION>", "definition", "1")
         TE.utils.testEncodeAttribute(self, self.obj, 'the_value', 'THE-VALUE')
-        TE.utils.testEncodeAttribute(self, self.obj, 'the_original_value', 'THE-ORIGINAL-VALUe')
+        TE.utils.testEncodeAttribute(self, self.obj, 'the_original_value', 'THE-ORIGINAL-VALUE')
         TE.utils.testEncodeAttribute(self, self.obj, 'is_simplified', 'IS-SIMPLIFIED')
 
 if __name__ == '__main__':
