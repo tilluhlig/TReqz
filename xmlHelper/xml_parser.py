03b6cffd49374128926cdab775638140eadc71d2
Till Uhlig <till.uhlig@gmx.net>
2019-12-29 17:14:46 +0100

    basic implementation for parseFile() and parse()

--- xmlHelper/xml_parser.py
+++ xmlHelper/xml_parser.py
@@ -1,3 +1,6 @@
+from xml.etree.ElementTree import Element
+from xml.etree.ElementTree import ElementTree
+import xml.etree.ElementTree as ET
 
 class xml_parser(object):
     """ this class represents a parser interface
@@ -6,11 +9,15 @@ class xml_parser(object):
     def __init__(self):
         pass
 
-    def parseFile(self, contentFile: str)->None:
-        raise NotImplementedError
+    def parseFile(self, contentFile: str)->Element:
+        file = open(contentFile, "r")
+        res = self.parse(file.read())
+        file.close()
+        return res
 
-    def parse(self, content: str)->None:
-        raise NotImplementedError
+    def parse(self, content: str)->Element:
+        root:Element = ET.fromstring(content)
+        return root
 
     def dump(self, content, pettyprint=True)->None:
         raise NotImplementedError
